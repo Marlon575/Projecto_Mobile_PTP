12d034a782a8482d71457a4ee837b7790acb4c59
Marlon <marlonmuchanga@gmail.com>
2026-05-19 17:41:15 +0100

    chore: criar ecrãs placeholder para todos os navigators

---
+++ Resuminho/view/screens/auth/LoginScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View,Text} from 'react-native';
+
+export default function LoginScreen (){
+    return(
+        <View style= {{flex:1, justityContent: 'center', alighItems: 'center'}}>
+            <Text>Login - em desenvolvimento</Text>
+        </View>
+    );
+}
