accdf9f517fefc3cdbb0b18714ccac9250599ea5
Marlon <marlonmuchanga@gmail.com>
2026-05-19 09:43:03 +0100

    feat: criar TabNavigato r

--- Resuminho/view/navigation/TabNavigator.jsx
+++ Resuminho/view/navigation/TabNavigator.jsx
@@ -1,5 +1,5 @@
 // Barra de tabs na parte inferior do ecrã
-// Adapta-se automaticamente ao tipo de utilizador (estudante ou revisor)
+// Adapta-se automaticamente ao tipo de utilizador estudante ou revisor
 
 import React, {use, useContext} from "react";
 
@@ -7,7 +7,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 //Cria a barra de navegacao na parte de baixo do ecra
 
 import {Ionicons} from '@expo/vector-icons';
-// Ionicons — biblioteca de ícones incluída no Expo
+// Ionicons — biblioteca de ícones do Expo
 
 import{AuthContext} from '../../viewmodel/context/AuthContext';
 
@@ -30,16 +30,14 @@ const { user } = useContext(AuthContext);
     return (
     <Tab.Navigator
     screenOptions={({ route }) => ({
-        // route — objecto com o nome da tab activa
-        // Esta função corre para cada tab individualmente
 
         headerShown: false,
 
         tabBarActiveTintColor: '#2563EB',
-        // Cor do ícone e texto quando a tab está seleccionada (azul)
+        // Cor do ícone e texto quando a tab está seleccionada 
 
         tabBarInactiveTintColor: '#9CA3AF',
-        // Cor quando a tab não está seleccionada (cinzento claro)
+        // Cor quando a tab não está seleccionada 
 
         tabBarStyle: {
         backgroundColor: '#FFFFFF',
@@ -48,18 +46,17 @@ const { user } = useContext(AuthContext);
         paddingBottom: 5,
         height: 60,
         },
-        // Estilo da barra em si — fundo branco, linha fina no topo
 
         tabBarIcon: ({ focused, color, size }) => {
           // focused — true se esta tab estiver activa
           // color — recebe automaticamente activeTintColor ou inactiveTintColor
-          // size — tamanho padrão do ícone definido pelo React Navigation
+          // size — tamanho padrão do ícone definida em React Navigation
 
         let iconName;
 
         if (route.name === 'Inicio') {
             iconName = focused ? 'home' : 'home-outline';
-            // Ícone preenchido quando activo, contorno quando inactivo
+
         } else if (route.name === 'Pesquisar') {
             iconName = focused ? 'search' : 'search-outline';
         } else if (route.name === 'Perfil') {
@@ -71,19 +68,19 @@ const { user } = useContext(AuthContext);
         }
 
         return <Ionicons name={iconName} size={size} color={color} />;
-          // Devolve o componente do ícone com as propriedades correctas
+    
         },
     })}
     >
     {user?.tipo === 'estudante' ? (
-        // Operador ternário: se estudante → mostra estas 3 tabs
+        
         <>
         <Tab.Screen name="Inicio" component={HomeEstudanteScreen} />
         <Tab.Screen name="Pesquisar" component={PesquisarScreen} />
         <Tab.Screen name="Perfil" component={PerfilScreen} />
         </>
     ) : (
-        // Se não for estudante (é revisor) → mostra estas 3 tabs
+        // Se não for estudante é revisor  mostra estas 3 tabs
         <>
         <Tab.Screen name="Inicio" component={HomeRevisorScreen} />
         <Tab.Screen name="Publicar" component={PublicarScreen} />
