d3dce98ece069f00be418dc4059c9dabea7a596c
Marlon <marlonmuchanga@gmail.com>
2026-05-20 11:23:21 +0100

    feat: adicionar disciplinas reais do curso de Engenharia Informática

---
+++ Resuminho/view/constants/disciplinas.js
@@ -0,0 +1,18 @@
+const disciplinas = [
+  {
+    id: '1',
+    nome: 'Engenharia Informática',
+    sigla: 'EI',
+    disciplinas: [
+      { id: '1-1', nome: 'Programação Web Mobile', ano: 1 },
+      { id: '1-2', nome: 'Sistemas de Gestão de Base de Dados', ano: 1 },
+      { id: '1-3', nome: 'Redes de Computadores', ano: 2 },
+      { id: '1-4', nome: 'Sistemas Operativos I', ano: 2 },
+      { id: '1-5', nome: 'Sistemas Operativos II', ano: 2 },
+      { id: '1-6', nome: 'Práticas de Técnicas de Programação', ano: 4 },
+      { id: '1-7', nome: 'Inglês Técnico', ano: 4 },
+    ],
+  },
+];
+
+module.exports = disciplinas;
