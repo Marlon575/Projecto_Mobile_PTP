b09be27e1308c5b3f19e816cc030789abf61c3b9
Marlon <marlonmuchanga@gmail.com>
2026-05-20 10:17:26 +0100

    chore: criar estrutura de pastas do backend

---
+++ Resuminho/backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require ('cors');
+const dotenv = require ('dotenv');
+const ligarBD = require('./model/config/database');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+const adminRoutes = require('./model/routes/adminRoutes');
+app.use('/admin', adminRoutes);
+
+// Rota de teste
+app.get('/', (req, res) => {
+res.json({ mensagem: 'Servidor Resuminho a funcionar!' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+ligarBD().then(() => {
+app.listen(PORT, () => {
+    console.log(`Servidor a correr na porta ${PORT}`);
+});
+});
